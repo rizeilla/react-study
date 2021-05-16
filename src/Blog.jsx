1baebb0c77f74ff75c89656ad703b6fa81e853f3
Floria Torii <floria@FlorianoMacBook-puro.local>
2021-05-16 13:56:49 +0900

    stateの使い方

--- src/Blog.jsx
+++ src/Blog.jsx
@@ -4,14 +4,21 @@ import Article from './Article';
 class Blog extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            isPublished: false
+        }
+
     }
+    // 公開状態を反転させる関数
+    togglePublished = () => {
+        this.setState({
+            isPublished: !this.state.isPublished
+        })
+    };
     render() {
-        const authorName = "ぐわ";
         return (
             <>
-                <Article title={"Reactの使い方"} order={1}/>
-                <Article title={"JSXの使い方"} order={2}/>
-                <Article title={"環境構築"} order={3}/>
+                <Article title={"Reactの使い方"} isPublished={this.state.isPublished} toggle={() => this.togglePublished()} />
             </>
 
         )
